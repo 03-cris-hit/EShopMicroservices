983085310f2c443c147ae45a0988919957c831a6
Cristobal Pérez <cperez@be-it.com.mx>
2025-08-05 20:23:33 -0600

    feat: integrate Carter, Mapster, MediatR, and Marten for API architecture and data access
    - Added Carter for modular API endpoint definitions
    - Added MediatR to implement CQRS pattern (Commands/Queries)
    - Added Mapster for efficient object mapping (DTO <-> Domain)
    - Integrated Marten for PostgreSQL document storage and session management
    - Configured global usings for cleaner syntax across the solution

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Catalog.API.Products.GetProductByCategory;
+
+public record GetProductByCategoryQuery(string categpry): IQuery<GetProductByCategoryResult>;
+public record GetProductByCategoryResult(IEnumerable<Product> Products);
+internal class GetProductByCategoryHandler(IDocumentSession session, ILogger<GetProductByCategoryHandler> logger)
+    : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+{
+    public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByCategoryHandler Called with {@query}", query);
+        var products = await session.Query<Product>().Where(p=>p.Category.Contains(query.categpry)).ToListAsync();
+        return new GetProductByCategoryResult(products);    
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductEnpoint.cs
@@ -0,0 +1,27 @@
+﻿
+using Catalog.API.Products.GetProductById;
+
+namespace Catalog.API.Products.UpdateProduct;
+
+public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+
+public record UpdateProductResponse(bool isSuccess);
+public class UpdateProductEnpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/products", async (UpdateProductRequest resquest, ISender sender) =>
+        {
+
+            var command = resquest.Adapt<UpdateProductCommand>();
+            var result = await sender.Send(command);
+            var resupose = result.Adapt<UpdateProductResponse>();
+            return Results.Ok(resupose);
+        }).WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Product")
+        .WithDescription("Update Product");
+    }
+}
+
