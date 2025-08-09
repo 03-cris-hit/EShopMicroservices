e47ff7d0c51790b3be952cae7c4dcb2acb22fc5f
Cristobal Pérez <cperez@be-it.com.mx>
2025-08-08 19:08:33 -0600

    feat(basket): implementación del servicio Basket.API y configuración de entorno
    - Se agregaron controladores, handlers y repositorios para manejo de canastas (Basket).
    - Se configuraron endpoints para crear, obtener, actualizar y eliminar canastas.
    - Se añadieron modelos, excepciones y clases de dominio relacionadas.
    - Se actualizó docker-compose y configuraciones de entorno (appsettings) para el nuevo servicio.
    - Ajustes en Catalog.API para integración con Basket.API.

---
+++ src/Services/Basket/Basket.API/Basket/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+namespace Basket.API.Basket.DeleteBasket;
+
+public record DeleteBasketCommand(string UserName): ICommand<DeleteBasketResult>;
+
+public record DeleteBasketResult(bool IsSuccess);
+public class DeleteBasketValidator : AbstractValidator<DeleteBasketCommand>
+{
+    public DeleteBasketValidator() {
+        RuleFor(x => x.UserName).NotNull().WithMessage("Username is required");
+    }
+}
+public class DeleteBasketCommandHandler(IBasketRepository repository) 
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+{
+    public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+    {
+        var result = await repository.Deletebasket(command.UserName, cancellationToken);
+        return new DeleteBasketResult(true);
+    }
+}
