553eeb18f181834d270289577bfdbb71404ae927
Cristobal Pérez <03-cris-hit@users.noreply.github.com>
2025-08-08 19:55:46 -0600

    Include GlobalUsing

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,12 @@
+﻿global using Mapster;
+global using MediatR;
+global using BuildingBlocks.CQRS;
+global using FluentValidation;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions.Handler;
+global using Basket.API.Models;
+global using Carter;
+global using Marten;
+global using BuildingBlocks.Exceptions;
+global using Basket.API.Data;
+ 
